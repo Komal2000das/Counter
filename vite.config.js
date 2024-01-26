0c9b834f961e59f7530c2aa80e8f885ca179e4db
Komal2000das <119794374+Komal2000das@users.noreply.github.com>
2024-01-27 00:33:59 +0530

    first commit

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
